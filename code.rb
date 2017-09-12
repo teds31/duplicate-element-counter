020aaa845180ab361cd5b44ad64042ecb1b74b30
Teddy Stanowski <tstanowski31@gmail.com>
2017-09-12 14:24:54 -0700

    count duplicates

---
+++ code.rb
@@ -0,0 +1,28 @@
+#YOUR CODE GOES HERE
+def duplicate_counter(array)
+
+  counts = {}
+  duplicates = 0
+
+  array.each do |item|
+    if counts[item].nil?
+      counts[item] = 1
+    else
+      counts[item] += 1
+    end
+  end
+
+  array.uniq.each do |item|
+    if counts[item] > 1
+      duplicates += counts[item] - 1
+    end
+  end
+
+  puts duplicates
+end
+
+duplicate_counter([1, 7, 7, 7, 3, 5])
+
+duplicate_counter([5, 7, 9])
+
+duplicate_counter([0,-5,-5,33,33,33])
